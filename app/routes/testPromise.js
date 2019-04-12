16b37ef78712b17b5846ac44a6e730ca25534b2a
zhaoqinglong <1031432339@qq.com>
2019-04-12 09:47:35 +0800

    update sequelize version

--- app/routes/testPromise.js
+++
@@ -1,9 +0,0 @@
-// const promise = new Promise(function(resolve, reject) {
-//   // ... some code
-
-//   if (/* 异步操作成功 */){
-//     resolve(value);
-//   } else {
-//     reject(error);
-//   }
-// });
